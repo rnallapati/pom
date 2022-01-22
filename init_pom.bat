6f84f8787396599f7c30460710dcacaaff2782fd
Raj Nallapati <raj@rajMac.lan>
2022-01-22 07:28:27 -0500

    Modify batch file to add .gitkeep files

--- init_pom.bat
+++ init_pom.bat
@@ -9,6 +9,11 @@ mkdir ./Reports
 type nul > ./pageObjects/__init__.py
 type nul > ./testCases/__init__.py
 type nul > ./utilities/__init__.py
+type nul > ./TestData/.gitkeep
+type nul > ./Configurations/.gitkeep
+type nul > ./Logs/.gitkeep
+type nul > ./Screenshots/.gitkeep
+type nul > ./Reports/.gitkeep
 python -m venv env
 source ./env/bin/activate
 pip install --upgrade pip
